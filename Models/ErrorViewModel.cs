29de1e085c9c837794396cf9dcc2ef0401a3cffe
Komal Malkan <200540107130@darshan.ac.in>
2023-02-25 23:52:30 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rapid_Road_Safety_Tools.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
